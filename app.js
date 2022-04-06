ad593f183c9cc57a610785492960f020c4ee2079
Rural Anemone <64103005+RuralAnemone@users.noreply.github.com>
2022-04-06 17:43:25 +0000

    oh goodness help particles :despair:

---
+++ app.js
@@ -0,0 +1,6 @@
+//fake backend idk
+particlesJS = require('particles.js');
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'assets/particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
