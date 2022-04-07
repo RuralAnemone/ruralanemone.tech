ceecd4db145390acf8a40e78482da45529649b3b
Rural Anemone <64103005+RuralAnemone@users.noreply.github.com>
2022-04-07 15:18:11 +0000

    probably fixed particles I have no idea

--- app.js
+++
@@ -1,6 +0,0 @@
-//fake backend idk
-particlesJS = require('particles.js');
-/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', 'assets/particles.json', function() {
-  console.log('callback - particles.js config loaded');
-});
