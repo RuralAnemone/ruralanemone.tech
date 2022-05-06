a59731e79de96e61ec55fcaaf375260b2e69010f
Rural Anemone <64103005+RuralAnemone@users.noreply.github.com>
2022-05-06 19:50:59 +0000

    what am I even doing

--- sites/script.js
+++ sites/script.js
@@ -1,9 +1,1 @@
-function m3 () {
-
-  var tab = window.open('about:blank', '_blank');
-
-  tab.document.documentElement.innerHTML = '<!DOCTYPE html><html><head><style>body {margin:0;overflow:hidden}</style></head><body><iframe width="100%" height="100%" src="https://' + window.location.hostname + '/mother3" frameborder="0"></iframe></body></html>';
-
-  tab.document.close();
-
-}
+//what
