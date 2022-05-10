e36dee3c6879527c65599173fb12e507ce1571e8
Rural Anemone <64103005+RuralAnemone@users.noreply.github.com>
2022-05-10 20:02:10 +0000

    feature requests for /sites

--- sites/script.js
+++ sites/script.js
@@ -1,1 +1,48 @@
-//what
+function notif(title,body,icon,open){
+// hi lol I just took this from mdn
+(async () => {
+    // create and show the notification
+    const showNotification = () => {
+        // create a new notification
+        const notification = new Notification(title, {
+            body: body,
+            icon: icon
+        });
+
+        // close the notification after 10 seconds
+        setTimeout(() => {
+            notification.close();
+        }, 10 * 1000);
+
+        // navigate to a URL when clicked
+        notification.addEventListener('click', () => {
+
+            window.open(open, '_blank');
+        });
+    }
+
+    // show an error message
+    const showError = () => {
+        const error = document.querySelector('.error');
+        error.style.display = 'block';
+        error.textContent = 'bruh what? allow notifications please';
+        alert("You blocked the notifications! 😭");
+        document.cookie="notifs=blocked";
+    }
+
+    // check notification permission
+    let granted = false;
+
+    if (Notification.permission === 'granted') {
+        granted = true;
+    } else if (Notification.permission !== 'denied') {
+        let permission = await Notification.requestPermission();
+        granted = permission === 'granted' ? true : false;
+    }
+
+    // show notification or error
+    granted ? showNotification() : showError();
+
+})();}
+
+notif("suggestions?","have any suggestions? click here to submit them!","","https://github.com/RuralAnemone/ruralanemone.tech/issues/new?assignees=RuralAnemone&labels=enhancement&template=feature-request.md&title=%5BFEATURE+REQUEST%5D")
